27715940dc9918220e6aabcfbd8adcc0b1b391cd
Felipe Galobart Jora <felipe@mitsuwa.com.br>
2023-07-26 17:34:28 -0300

    criado as funcoes de listar e criar

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -2,11 +2,19 @@ import { Request, Response } from 'express';
 import { Todo } from '../models/Todo';
 
 export const all = async (req: Request, res: Response) => {
-
+    let list = await Todo.findAll();
+    res.json({list})
 }
 
 export const add = async (req: Request, res: Response) => {
-    
+    let title: string = req.body.title;
+    let done: boolean = req.body.done;
+    let newTodo = await Todo.create({
+        title,
+        done
+    })
+    res.status(201);
+    res.json({id: newTodo.id, title, done});
 }
 
 export const update = async (req: Request, res: Response) => {
