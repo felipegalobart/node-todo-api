d45d8a1c448cabbdcfb43e9032974129883bfaeb
Felipe Galobart Jora <felipe@mitsuwa.com.br>
2023-07-26 17:08:35 -0300

    criando as rotas

---
+++ src/controllers/todo.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { Todo } from '../models/Todo';
+
+export const all = async (req: Request, res: Response) => {
+
+}
+
+export const add = async (req: Request, res: Response) => {
+    
+}
+
+export const update = async (req: Request, res: Response) => {
+    
+}
+
+export const remove = async (req: Request, res: Response) => {
+    
+}
